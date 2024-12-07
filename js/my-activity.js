5cc3a95148778c10561c89262dfa4287d30d65a1
Punsiri Boonyakiat <punsiri.boo@gmail.com>
2024-12-07 12:37:46 +0700

    Add more speakerdesk

--- js/my-activity.js
+++ js/my-activity.js
@@ -6,11 +6,10 @@ const activities = [
         description:
             'As an instructor, hosted a 2-day offline hands-on class focusing on modern Data and AI technologies. Tools included <b>Docker, Apache Airflow, Google Cloud Storage, BigQuery, Gemini Code Assist, Vertex AI Agent Builder, and LINE Messaging API.</b> The bootcamp emphasized building modern data pipelines, accelerating development with GenAI tools, and creating AI agents for seamless data interaction.',
         links: [
-            { text: 'Facebook Link', href: '#' },
-            { text: 'GitHub', href: '#' },
-            { text: 'Speakerdeck', href: '#' },
+            { text: 'Event Link', href: 'https://www.eventpop.me/e/60322/data-ai-boostcamp?fbclid=IwY2xjawHAvbdleHRuA2FlbQIxMAABHXtBmKaHBoWCanh4i-nLvL7aOiunjejYTOQ5KpXeUC9knRkKjsufynu5vA_aem_Ip4B2w9f6_8F0hG7Y79wtQ' },
+            { text: 'GitHub', href: 'https://github.com/wuttichai-hung/data-ai-bootcamp' },
         ],
-        embedResource: "",
+        embedResource: "<iframe class='speakerdeck-iframe' frameborder='0' src='https://speakerdeck.com/player/0667ef6003cc425aaf957772659adab9' title='[Data เบ็ดเตล็ด x DataHungry] Data &amp; AI Bootcamp 2024' allowfullscreen='true' style='border: 0px; background: padding-box padding-box rgba(0, 0, 0, 0.1); margin: 0px; padding: 0px; border-radius: 6px; box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 40px; width: 100%; height: auto; aspect-ratio: 560 / 315;' data-ratio='1.7777777777777777'></iframe>",
     },
     {
         title: 'GDG DevFest Bangkok 2024 - The Future of Retail E-commerce with Gemini AI',
@@ -18,8 +17,7 @@ const activities = [
         description:
             'Showcased how <b>Vertex AI Agent Builder, Vertex AI Search and Gemini Multimodal</b> enhance retail e-commerce using <b>Retrieval-Augmented Generation (RAG)</b>. Demonstrated LINE Chatbot features including general product inquiries, text-based product search, and image-based product search.',
         links: [
-            { text: 'Facebook Link', href: '#' },
-            { text: 'Speakerdeck', href: '#' },
+            { text: 'Event Link', href: 'https://shop.line.me/@apppi/product/1006936241' },
         ],
         embedResource:"<iframe class='speakerdeck-iframe' frameborder='0' src='https://speakerdeck.com/player/a3d0876c197846ba8ed1965f372dcd1c' title='[GDG DevFest Bangkok 2024] - The Future of Retail E-commerce with Gemini AI' allowfullscreen='true' style='border: 0px; background: padding-box padding-box rgba(0, 0, 0, 0.1); margin: 0px; padding: 0px; border-radius: 6px; box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 40px; width: 100%; height: auto; aspect-ratio: 560 / 315;' data-ratio'1.7777777777777777'></iframe>"
     },
@@ -30,8 +28,6 @@ const activities = [
             'Similar to GDG DevFest, demonstrated <b>Vertex AI Agent Builder, Vertex AI Search and Gemini</b>’s capabilities for retail e-commerce, focusing on enhancing user experience through <b>Retrieval-Augmented Generation (RAG)</b> and interactive chatbot features.',
         links: [
             { text: 'Speakerdeck', href: 'https://speakerdeck.com/linedevth/the-future-of-line-retail-chatbot-using-gemini-ai' },
-            { text: 'Facebook Link', href: '#' },
-            { text: 'Event Link', href: '#' },
         ],
         embedResource: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Saw5M6UuSzg?si=wQjnq2kIBjjJK8ob" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     },
@@ -80,27 +76,27 @@ const activities = [
         title: 'Data เบ็ดเตล็ด Live (965 followers) - Gemini in BigQuery and BigQuery Data Canvas',
         date: '13 Aug 2024',
         description:
-            'Demonstrated Gemini in BigQuery for query automation and BigQuery Data Canvas for visual data modeling.',
+            'Demonstrated <b>Gemini in BigQuery</b> for query generation and completio. I also demonstrate <b>BigQuery Data Canvas</b> for data explository analysis.',
         links: [{ text: 'Facebook Live Link', href: '#' }],
-        embedResource: "",
+        embedResource: "<img src='images/portfolio/data-bed-1.png' alt='img'>",
     },
     {
         title: '[Data TH - Data Science ชิลชิล 65K followers] Spacial Class - Gemini Code Assist for Data Engineering - Road to Data Engineer',
         date: '23 June 2024',
         description:
-            'The workshop introduced Gemini Code Assist, an AI-powered tool designed to automate data engineering tasks. During the session, I guided participants through setting up Gemini Code Assist in VS Code, BigQuery, Docker to create and manage Airflow DAGs effectively. Additionally, I demonstrated how to craft prompts for generating code, writing unit tests, creating diagrams/documentation, and validating data using Great Expectations. The session featured live demonstrations and provided hands-on practice on Gemini Code Assist.',
+            'The workshop introduced <b>Gemini Code Assist</b>, an AI-powered tool designed to automate data engineering tasks. During the session, I guided participants through setting up Gemini Code Assist in VS Code, BigQuery, Docker to create and manage Airflow DAGs effectively. Additionally, I demonstrated how to craft prompts for generating code, writing unit tests, creating diagrams/documentation, and validating data using Great Expectations. The session featured live demonstrations and provided hands-on practice on Gemini Code Assist.',
         links: [
-            { text: 'Link to Poster for DATA TH', href: '#' },
+            { text: 'Linkedin Post', href: 'https://www.linkedin.com/feed/update/urn:li:activity:7210490174567325696/' },
         ],
-        embedResource: "",
+        embedResource: "<img src='images/portfolio/r2de-2.jpg' alt='img'>",
     },
     {
-        title: '[Chaiyo GCP - Online Code Along Session] - Gemini for Data Scientist [Running Codelab]',
+        title: '[Cloud AI Study Jam 2024 - Chaiyo GCP - Online Code Along Session] - Gemini for Data Scientist [Running Codelab]',
         date: '28 May 2024',
         description:
-            'The lab session at the Chaiyo GCP Event in Thailand focuses on using Gemini in BigQuery as an assistant to build a K-means clustering model. As a speaker, I guided participants through segmenting customers based on their order behaviors by writing prompts for Data Scientists’s workflow.',
+            'The lab session at the Chaiyo GCP Event in Thailand focuses on using <b>Gemini in BigQuery</b> as an assistant to build a K-means clustering model. As a speaker, I guided participants through segmenting customers based on their order behaviors by writing prompts for Data Scientists’s workflow.',
         links: [
-            { text: 'Chaiyo GCP Cloud Bangkok Event Link', href: ' https://gdg.community.dev/events/details/google-gdg-cloud-bangkok-presents-chaiyogcp-season-4-code-along-session-online/' },
+            { text: 'GCP Cloud Bangkok Event Link', href: ' https://gdg.community.dev/events/details/google-gdg-cloud-bangkok-presents-chaiyogcp-season-4-code-along-session-online/' },
         ],
         embedResource: "<iframe width='560' height='315' src='https://www.youtube.com/embed/NUFVP-bAi_s?si=nnR645IJ47m69UCL' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>",
     },
@@ -113,18 +109,18 @@ const activities = [
         links: [
             { text: 'Facebook Link', href: '#' },
         ],
-        embedResource: "",
+        embedResource: "<img src='images/portfolio/r2de-1.jpg' alt='img'>",
     },
     {
 
         title: '[National Telecom Data & AI Hackathon] - Generative AI with Gemini',
         date: '23 April 2024',
         description:
-            'NT Academy arranged an internal hackathon about Data, AI, and ML. As a Hackathon mentor, I had a chance to teach ML unsupervised techniques and also generative AI. The GenAI is based on Gemini Multimodal. The session included hands-on teaching on multimodal prompts, best practices in prompt design, and techniques to write efficient prompts using Gemini AI for maximum effectiveness.',
+            'As a Hackathon mentor, I had a chance to teach ML unsupervised techniques and also generative AI. The Generative AI is based on <b>Gemini Multimodal</b>. The session included hands-on teaching on multimodal prompts, best practices in <b>Prompt design, and techniques to write efficient prompts using Gemini</b> AI for maximum effectiveness.',
         links: [
             { text: 'Linkedin Post', href: '#' },
         ],
-        embedResource: "",
+        embedResource: "<iframe class='speakerdeck-iframe' frameborder='0' src='https://speakerdeck.com/player/f4ea599b819d4f66b9b4f38acf907f29' title='National Telecom Data &amp; AI Hackathon ' allowfullscreen='true' style='border: 0px; background: padding-box padding-box rgba(0, 0, 0, 0.1); margin: 0px; padding: 0px; border-radius: 6px; box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 40px; width: 100%; height: auto; aspect-ratio: 560 / 315;' data-ratio='1.7777777777777777'></iframe>",
     },
     {
 
@@ -153,8 +149,9 @@ const activities = [
         title: '[LINE DEV CONFERENCE 2021] - Applied Machine Learning in Running Assistance Bot',
         date: 'November 2021',
         description:
-            'The talk explains applying ML technology to the LINE chatbot. The Running Assistant chatbot is used as an example use-case. The solution is built on the Google Cloud Platform (GCP) for ease of use and machine learning capabilities. Key components include LINE Messaging API for webhook interactions, Google Cloud Functions for event handling, Dialogflow for conversational flow, Cloud Vision API for OCR, Firestore for data storage, Cloud Logging for log management, BigQuery for data analytics, and Data Studio for visualization. These tools work together to create a robust chatbot experience for virtual running events.',
+            'The talk explains applying ML technology to the LINE chatbot. The Running Assistant chatbot is used as an example use-case. The solution is built on the Google Cloud Platform (GCP) for ease of use and machine learning capabilities. Key components include LINE Messaging API for webhook interactions, Google Cloud Functions for event handling, <b>Dialogflow</b> for conversational flow, <b>Cloud Vision API</b> for OCR, <b>Firestore</b> for data storage, <b>Cloud Logging</b> for log management, BigQuery for data analytics, and <b>Data Studio</b> for visualization. These tools work together to create a robust chatbot experience for virtual running events.',
         links: [
+            { text: 'Speakerdeck', href: 'https://speakerdeck.com/linedevth/applied-machine-learning-in-running-assistance-bot' },
             { text: 'Facebook Event Link', href: 'https://www.facebook.com/events/d41d8cd9/line-thailand-developer-conference-2021/3146377905633688/' },
             { text: 'Facebook Live Link', href: 'https://www.facebook.com/LINEDEVTH/videos/903319493611003' },
             { text: 'Medium Link', href: 'https://medium.com/linedevth/running-assistance-chatbot-%E0%B8%8A%E0%B8%A7%E0%B8%99%E0%B8%97%E0%B8%B3%E0%B8%9A%E0%B8%AD%E0%B8%97-virtual-run-%E0%B9%80%E0%B8%AD%E0%B8%B2%E0%B9%83%E0%B8%88%E0%B8%99%E0%B8%B1%E0%B8%81%E0%B8%A7%E0%B8%B4%E0%B9%88%E0%B8%87-1e3a2aa7d210' },
@@ -220,7 +217,8 @@ if (container) {
         title.appendChild(titleLink);
 
         const date = document.createElement('span');
-        date.textContent = activity.date;
+        date.classList.add('entry-activity-date');
+        date.textContent = "Date: " + activity.date;
 
         const description = document.createElement('p');
         description.classList.add('mb-2', 'fs-act');
